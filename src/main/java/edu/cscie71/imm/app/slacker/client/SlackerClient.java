08607501f2edc8c84045fccd983e0d77e2fbb7a6
Jeffry Pincus <jeffry.pincus@gmail.com>
2015-11-29 22:45:17 -0500

    [SKCP-85] Mocking Slack API service.

--- src/main/java/edu/cscie71/imm/app/slacker/client/SlackerClient.java
+++ src/main/java/edu/cscie71/imm/app/slacker/client/SlackerClient.java
@@ -4,24 +4,34 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.fluent.Request;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.fluent.Form;
+
 import java.util.List;
 import java.io.IOException;
+import java.net.URLEncoder;
 
 public class SlackerClient implements ISlackerClient {
 
-    private static final String MESSAGE_URL = "https://slack.com/api/chat.postMessage";
-    private static final String USER_DATA_URL = "https://slack.com/api/users.info";
+    private static final String MESSAGE = "/chat.postMessage";
+    private static final String USER_DATA = "/api/users.info";
+    private final String BASE_URL;
+
+    public SlackerClient() {
+        BASE_URL = "https://slack.com/api";
+    }
+
+    protected SlackerClient(String baseURL) {
+        BASE_URL = baseURL;
+    }
 
     /**
      * @inheritDoc
      */
     public String postMessage(String token, String channel, String message) {
-        Form form = Form.form();
-        form.add("token", token)
-            .add("channel", channel)
-            .add("text", message)
-            .add("as_user", "true");
-        return postToURL(MESSAGE_URL, form.build());
+        String queryString = "?token=".concat(URLEncoder.encode(token))
+                .concat("&channel=").concat(URLEncoder.encode(channel))
+                .concat("&text=").concat(URLEncoder.encode(message))
+                .concat("&as_user=").concat("true");
+        return postToURL(BASE_URL.concat(MESSAGE).concat(queryString));
     }
 
     /**
@@ -30,11 +40,26 @@ public class SlackerClient implements ISlackerClient {
     public String getUserInfo(String token, String user) {
         Form form = Form.form();
         form.add("token", token).add("user", user);
-        return postToURL(USER_DATA_URL, form.build());
+        String queryString = "?token=".concat(URLEncoder.encode(token))
+                .concat("&user=").concat(URLEncoder.encode(user));
+        return getFromURL(BASE_URL.concat(USER_DATA).concat(queryString));
     }
 
-    private String postToURL(String url, List<NameValuePair> formData) {
-        Request toSend = Request.Post(url).bodyForm(formData);
+    private String getFromURL(String url) {
+        Request toSend = Request.Get(url);
+        try {
+            return toSend.execute().returnContent().asString();
+        }
+        catch (ClientProtocolException e) {
+            return "{\"ok\":false,\"error\":\"client_protocol_exception: " + e.getMessage() + "\"}";
+        }
+        catch (IOException e) {
+            return "{\"ok\":false,\"error\":\"io_exception: " + e.getMessage() + "\"}";
+        }
+    }
+
+    private String postToURL(String url) {
+        Request toSend = Request.Post(url);
         try {
             return toSend.execute().returnContent().asString();
         }
--- src/test/java/edu/cscie71/imm/app/slacker/client/SlackerClientTest.java
+++ src/test/java/edu/cscie71/imm/app/slacker/client/SlackerClientTest.java
@@ -1,35 +1,56 @@
 package edu.cscie71.imm.app.slacker.client;
 
 import org.junit.*;
+import com.github.restdriver.clientdriver.ClientDriverRequest.Method;
+import com.github.restdriver.clientdriver.ClientDriverRule;
+
+import static com.github.restdriver.clientdriver.RestClientDriver.*;
 
 public class SlackerClientTest {
 
-    ISlackerClient slack;
+    ISlackerClient mockSlack;
     String token = "xoxp-10020492535-10036686290-14227963249-1cb545e1ae";
     String immTestChannel = "C0F6U0R5E";
     String message = "This is the Android test.";
 
+    @Rule
+    public ClientDriverRule clientDriver = new ClientDriverRule();
+
     @Before
     public void setUp() throws Exception {
-        slack = new SlackerClient();
+        mockSlack = new SlackerClient(clientDriver.getBaseUrl());
     }
 
     @Test
     public void testResponseIsOkAndContainsMessage() throws Exception {
-        String okMsgResponse = slack.postMessage(token, immTestChannel, message);
+        clientDriver.addExpectation(
+                onRequestTo("/chat.postMessage").withMethod(Method.POST)
+                        .withParam("as_user", "true")
+                        .withParam("token", token)
+                        .withParam("channel", immTestChannel)
+                        .withParam("text", message),
+                giveResponse("\"ok\":true,\"text\":\"This is the Android test.\"", "text/plain")
+        );
+        String okMsgResponse = mockSlack.postMessage(token, immTestChannel, message);
         Assert.assertTrue(okMsgResponse.contains("\"ok\":true"));
         Assert.assertTrue(okMsgResponse.contains("\"text\":\"This is the Android test.\""));
     }
 
     @Test
     public void testGetUserInfo() throws Exception {
-        String userInfo = slack.getUserInfo(token, "U0A12L68J");
+        clientDriver.addExpectation(
+                onRequestTo("/api/users.info").withMethod(Method.GET)
+                        .withParam("token", token)
+                        .withParam("user", "U0A12L68J"),
+                giveResponse("\"ok\":true,\"real_name\":\"Jeffry Pincus\"", "text/plain")
+        );
+        String userInfo = mockSlack.getUserInfo(token, "U0A12L68J");
         Assert.assertTrue(userInfo.contains("\"ok\":true"));
         Assert.assertTrue(userInfo.contains("\"real_name\":\"Jeffry Pincus\""));
     }
 
     @After
     public void tearDown() throws Exception {
-        slack = null;
+        mockSlack = null;
     }
 }
